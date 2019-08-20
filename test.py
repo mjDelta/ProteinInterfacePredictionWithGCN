af7f7d2e13df671fbb8042a820e2b3137da6ac8d
zmj <zmj_xy@sjtu.edu.cn>
2019-08-20 11:54:29 +0800

    auc:0.75 acc:0.69

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ USE_CUDA=torch.cuda.is_available()
 device=torch.device("cuda" if USE_CUDA else "cpu")
 
 data_path="E:/proteins/test.cpkl.gz"
-load_model_path="E:/proteins/saved_models_dim50/model_130.tar"
+load_model_path="E:/proteins/saved_models/model_137.tar"
 
 hidden_dim=50
 train_rate=0.
--- train.py
+++ train.py
@@ -15,6 +15,9 @@ def mkdirs(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
 
+def adjust_learning_rate(optimizer,lr):
+	for param in optimizer.param_groups:
+		param["lr"]=lr
 
 USE_CUDA=torch.cuda.is_available()
 device=torch.device("cuda" if USE_CUDA else "cpu")
@@ -22,10 +25,10 @@ device=torch.device("cuda" if USE_CUDA else "cpu")
 train_path="E:/proteins/train.cpkl.gz"
 saved_models="E:/proteins/saved_models";mkdirs(saved_models)
 epochs=200
-hidden_dim=30
+hidden_dim=50
 train_rate=0.8
 drop_prob=0.5
-lr=0.01
+lr=0.1
 weight_decay=5e-4
 batch_size=1024
 best_loss=9999
@@ -45,7 +48,13 @@ iter_losses=[]
 train_losses=[]
 val_losses=[]
 val_accs=[]
+no_update_cnter=0
 for e in range(epochs):
+	if no_update_cnter>=10:
+		lr*=0.1
+		adjust_learning_rate(optimizer,lr)
+		no_update_cnter=0
+
 	e_loss=0.
 	for g in train_graphs:
 
@@ -148,6 +157,10 @@ for e in range(epochs):
 			"optimizer":optimizer.state_dict()
 			},os.path.join(saved_models,"model_{}.tar".format(e)))
 		best_loss=val_losses[-1]
+		print("UPDATE\tEpoch {}: train loss {}\tval loss {}\tval acc{}".format(e,train_losses[-1],val_losses[-1],val_accs[-1]))
+		no_update_cnter=0
+	else:
+		no_update_cnter+=1
 	if e%10==0:
 		print("Epoch {}: train loss {}\tval loss {}\tval acc{}".format(e,train_losses[-1],val_losses[-1],val_accs[-1]))
 
